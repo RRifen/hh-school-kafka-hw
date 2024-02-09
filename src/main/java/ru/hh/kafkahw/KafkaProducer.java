fa584a9e65eed0b474ed6c432a66c153de0835f5
RRifen <arsn.bazhenov@gmail.com>
2024-02-09 15:04:04 +0300

    solve atMostOnce and atLeastOnce cases

--- src/main/java/ru/hh/kafkahw/KafkaProducer.java
+++ src/main/java/ru/hh/kafkahw/KafkaProducer.java
@@ -1,6 +1,8 @@
 package ru.hh.kafkahw;
 
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -17,14 +19,40 @@ public class KafkaProducer {
     this.kafkaTemplate = kafkaTemplate;
   }
 
+   /*
+   Проблема была в том, что исключения могли прервать отправку сообщений в брокер.
+   Из-за этого семантики AtLeastOnce и ExactlyOnce не работали.
+   В методе send мы повторяем отправку сообщения до тех пор, пока его не отправим.
+   */
   public void send(String topic, String payload) {
-    if (random.nextInt(100) < 10) {
-      throw new RuntimeException();
+    boolean isSent = false;
+    while(!isSent) {
+      isSent = trySend(topic, payload);
     }
-    LOGGER.info("send to kafka, topic {}, payload {}", topic, payload);
-    kafkaTemplate.send(topic, payload);
-    if (random.nextInt(100) < 2) {
-      throw new RuntimeException();
+  }
+
+  /*
+  Данный метод пытается отправить сообщение в кафку.
+  В коде могут возникнуть исключения до отправки сообщения и после.
+  Исключение, возникающее до отправки, мешало реализовать семантики,
+  а исключение после отправки возможно не мешало, так как отправка происходит асинхронно.
+  Но я также добавил блокирующий вызов get на CompletableFuture, чтобы быть точно уверенным,
+  что сообщение отправилось
+   */
+  private boolean trySend(String topic, String payload) {
+    boolean isSent = false;
+    try {
+      if (random.nextInt(100) < 10) {
+        throw new RuntimeException();
+      }
+      LOGGER.info("send to kafka, topic {}, payload {}", topic, payload);
+      kafkaTemplate.send(topic, payload).get();
+      isSent = true;
+      if (random.nextInt(100) < 2) {
+        throw new RuntimeException();
+      }
+    } catch(RuntimeException | ExecutionException | InterruptedException ignore) {
     }
+    return isSent;
   }
 }
--- src/main/java/ru/hh/kafkahw/TopicListener.java
+++ src/main/java/ru/hh/kafkahw/TopicListener.java
@@ -19,20 +19,41 @@ public class TopicListener {
     this.service = service;
   }
 
+  /*
+  Для семантики atMostOnce достаточно просто проигнорировать исключение.
+  Сообщение может не успеть обработаться, но это не страшно.
+  Исключение необходимо ловить в блоке try, потому что обработчик ошибок по умолчанию
+  заново прочитает сообщение из брокера, даже если успеть закоммитить offset (ack.acknowledge)
+   */
   @KafkaListener(topics = "topic1", groupId = "group1")
   public void atMostOnce(ConsumerRecord<?, String> consumerRecord, Acknowledgment ack) {
     LOGGER.info("Try handle message, topic {}, payload {}", consumerRecord.topic(), consumerRecord.value());
-    service.handle("topic1", consumerRecord.value());
     ack.acknowledge();
+    try {
+      service.handle("topic1", consumerRecord.value());
+    } catch(RuntimeException ignored) {
+    }
   }
 
+  /*
+  Для семантики atLeastOnce по сути надо было только исправить Producer.
+  Дефолтный обработчик ошибок сам вернет offset если вылезло исключение.
+  При этом одно и то же сообщение может обработаться более одного раза,
+  так как исключение может вылезти уже после обработки сообщения
+   */
   @KafkaListener(topics = "topic2", groupId = "group2")
   public void atLeastOnce(ConsumerRecord<?, String> consumerRecord, Acknowledgment ack) {
     LOGGER.info("Try handle message, topic {}, payload {}", consumerRecord.topic(), consumerRecord.value());
-    ack.acknowledge();
     service.handle("topic2", consumerRecord.value());
+    ack.acknowledge();
   }
 
+  /*
+  Здесь все сложно. Так как ошибка может возникнуть либо до, либо после обработки,
+  то без изменения класса Service не обойтись, можно реализовать тот же механизм ретраев, как в продюсере.
+  Либо можно реализовать семантику atLeastOnce, тогда вероятность того,
+  что сообщение обработается более одного раза будет 2% - random.nextInt(100) < 2.
+   */
   @KafkaListener(topics = "topic3", groupId = "group3")
   public void exactlyOnce(ConsumerRecord<?, String> consumerRecord, Acknowledgment ack) {
     LOGGER.info("Try handle message, topic {}, payload {}", consumerRecord.topic(), consumerRecord.value());
