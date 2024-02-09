7a320f811d7dd8527c89434c00561e4ac67a0a16
RRifen <arsn.bazhenov@gmail.com>
2024-02-09 15:13:54 +0300

    solve exactlyOnce case

--- src/main/java/ru/hh/kafkahw/internal/Service.java
+++ src/main/java/ru/hh/kafkahw/internal/Service.java
@@ -12,15 +12,33 @@ public class Service {
   private final ConcurrentMap<String, ConcurrentMap<String, AtomicInteger>> counters = new ConcurrentHashMap<>();
   private final Random random = new Random();
 
+  /*
+  Реализовал ту же логику, что и в продюсере. Ретраим до тех пор, пока не сделаем инкремент,
+  и ставим флаг об успешной обработке, до того как вызовется второе исключение.
+   */
   public void handle(String topic, String message) {
-    if (random.nextInt(100) < 10) {
-      throw new RuntimeException();
+    boolean isCompute = false;
+    while(!isCompute) {
+      isCompute = tryHandle(topic, message);
     }
-    counters.computeIfAbsent(topic, key -> new ConcurrentHashMap<>())
-        .computeIfAbsent(message, key -> new AtomicInteger(0)).incrementAndGet();
-    if (random.nextInt(100) < 2) {
-      throw new RuntimeException();
+  }
+
+  private boolean tryHandle(String topic, String message) {
+    boolean isCompute = false;
+    try {
+      if (random.nextInt(100) < 10) {
+        throw new RuntimeException();
+      }
+      counters.computeIfAbsent(topic, key -> new ConcurrentHashMap<>())
+              .computeIfAbsent(message, key -> new AtomicInteger(0)).incrementAndGet();
+      isCompute = true;
+      if (random.nextInt(100) < 2) {
+        throw new RuntimeException();
+      }
     }
+    catch(RuntimeException ignore) {
+    }
+    return isCompute;
   }
 
   public int count(String topic, String message) {
